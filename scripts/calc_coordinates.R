b119d415eba3407cb6c727cf3149569ec848d156
Christopher Bird <u.cb142096@llogin2.launch>
2024-07-09 11:41:23 -0500

    updat

--- scripts/calc_coordinates.R
+++ scripts/calc_coordinates.R
@@ -31,7 +31,15 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 #### READ IN DATA ####
 
 data <-
-  read_csv(data_path) %>%
+  read_csv(
+    data_path,
+    na = c(
+      "na",
+      "NA",
+      "",
+      "?"
+    )
+  ) %>%
   clean_names() %>%
   select(-starts_with("x")) %>%
   mutate(
@@ -39,9 +47,16 @@ data <-
     # https://chatgpt.com/share/8c1d97f4-8564-42e1-ae35-d11c47f0f473
     coord_x =
       case_when(
-        is.na(coord_x) & !is.na(coord_y) ~
+        is.na(coord_x) ~
           -(reference_coord_x + distance_m * cos(angle_radians)),
         TRUE ~
           coord_x
+      ),
+    coord_y =
+      case_when(
+        is.na(coord_y) ~
+          -(reference_coord_y + distance_m * sin(angle_radians)),
+        TRUE ~
+          coord_y
       )
   )
